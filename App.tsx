7e4967500c1d79af44b3f566c5dd92c898738f09
Eflowz <ezekielkachy@gmail.com>
2025-05-03 07:59:15 -0700

    innitial

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
+import { Text, View } from 'react-native';
 import './global.css';
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-lg font-bold text-blue-500">Hello, NativeWind!</Text>
+    </View>
   );
 }
+
